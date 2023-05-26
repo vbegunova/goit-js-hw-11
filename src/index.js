bda020e9ef8d9c9047c582eeb849de98e14a7ea1
vbegunova <veronikabegunova42@gmail.com>
2023-05-26 11:50:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -86,25 +86,25 @@ function createMarkup(arr) {
     comments,
     downloads}) => `<div class="photo-card">
     <a href="${largeImageURL}">
-      <img class="item-thumb" src="${webformatURL}" alt="${tags}" loading="lazy"></a>
+      <img class="item-thumb" src="${webformatURL}" alt="${tags}" loading="lazy">
       <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-    
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+    </a>
   </div>`).join('');
 }
