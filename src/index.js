733a0b8efecdbfdc577cbdc7f0f5d70cd2030f13
vbegunova <veronikabegunova42@gmail.com>
2023-05-28 22:44:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '36694393-8bc689be0a863a766f731264d';
-const PER_PAGE = 20;
+const PER_PAGE = 40;
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const guard = document.querySelector('.guard');
-let page;
+let page = 1;
 let query;
 
 const lightbox = new SimpleLightbox('.gallery a');
@@ -22,7 +22,6 @@ let options = {
 
 let observer = new IntersectionObserver(handlerPagination, options);
 
-
 form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
@@ -30,51 +29,63 @@ function handlerSubmit(evt) {
   page = 1;
   const { searchQuery } = evt.currentTarget.elements;
   query = searchQuery.value.trim();
-  searchImages(query) 
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again');
-      } else {
-        Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        gallery.innerHTML = createMarkup(data.hits);
-        lightbox.refresh();
-      }
-      if (data.totalHits > PER_PAGE) {
-            observer.observe(guard);
-        }
-    })
-    .catch(err => console.log(err));
-  form.reset();
+  if (!query) {
+    Notify.warning('Please input valid query');
+    gallery.innerHTML = '';
+    form.reset();
+    return;
+  } else {
+    searchImages(query, page);
+    form.reset();
+  }
 }
 
-async function searchImages(query, page = 1) {
-  const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`);
-  return response;
+async function searchImages(query, page) {
+  try {
+    console.log(page);
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`);
+    if (response.data.totalHits === 0) {
+      Notify.failure('Sorry, there are no images matching your search query. Please try again');
+    } else if (page === 1) {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      gallery.innerHTML = createMarkup(response.data.hits);
+    } else {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(response.data.hits));
+    }
+    lightbox.refresh();
+      
+    const { height: cardHeight } = document
+      .querySelector(".gallery")
+      .firstElementChild.getBoundingClientRect();
+    
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
+    
+    if (response.data.totalHits > PER_PAGE) {
+      observer.observe(guard);
+    }
+    
+    if (response.data.totalHits <= page * PER_PAGE) {
+      Notify.warning("We're sorry, but you've reached the end of search results.");
+      observer.unobserve(guard);
+    }
+  } catch(err) {
+    console.log(err.message);
+  }
 }
 
 function handlerPagination(entries, observer) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            page += 1;
-            searchImages(query, page)
-                .then(({data}) => {
-                  gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-                  lightbox.refresh();
-                  const { height: cardHeight } = document
-                  .querySelector(".gallery")
-                  .firstElementChild.getBoundingClientRect();
-                  window.scrollBy({
-                    top: cardHeight * 2,
-                    behavior: "smooth",
-                  });
-                  if (data.totalHits <= page * PER_PAGE) {
-                      Notify.warning("We're sorry, but you've reached the end of search results.");
-                      observer.unobserve(guard);
-                    }
-                })
-                .catch(err => console.log(err));
-        }
-    })
+  if (!query) {
+    return;
+  }
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      page += 1;
+      searchImages(query, page);
+    }
+  });
 }
 
 function createMarkup(arr) {
