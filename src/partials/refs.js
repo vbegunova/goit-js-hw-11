73b367d6c002cd7ca44ae2f3c6ca2a03da6ab4c4
vbegunova <veronikabegunova42@gmail.com>
2023-05-30 19:12:14 +0300

    Create refs.js

---
+++ src/partials/refs.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const guard = document.querySelector('.guard');
+
+export { form, gallery, guard };
